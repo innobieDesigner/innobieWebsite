b0118c9166e5d9c755ffd0e054d80e5dc2db4cce
Konrad Kavai <40326331+innobieDesigner@users.noreply.github.com>
2018-06-21 23:22:14 +0200

    New design - not responsive

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// const langSelect = document.getElementsByTagName("p").getAttribute("lang");
+// const langHun = document.getElementsByTagName("p").getAttribute("lang");
+const langEng = document.getElementsByClassName("langEng");
+const langHun = document.getElementsByClassName("langHun");
+const butt = document.getElementsByClassName('button');
+
+function cte() {
+    for (var i = 0; i < langEng.length; i++) {
+        if(langEng[i] = butt) {
+            langEng[i].style.display = 'inline-block';
+        }
+        else {
+            langEng[i].style.display = 'block';
+        }
+    }
+    for(var i = 0; i < langHun.length; i++) {
+        langHun[i].style.display = 'none';
+    }
+}
+
+function cth() {
+    for (var i = 0; i < langHun.length; i++) {
+        if(langHun[i] = butt) {
+            langHun[i].style.display = 'inline-block';
+        }
+        else{
+            langHun[i].style.display = 'block';
+        }
+
+    }
+    for(var i = 0; i < langEng.length; i++) {
+        langEng[i].style.display = 'none';
+    }
+}
